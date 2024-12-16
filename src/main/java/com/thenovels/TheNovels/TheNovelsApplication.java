2f380392b77faa1636a908fea0f145b4433ca654
Kokfong Hav <kokfonghav@Kokfongs-MacBook-Pro.local>
2024-12-16 21:57:47 +0700

    First Commit

---
+++ src/main/java/com/thenovels/TheNovels/TheNovelsApplication.java
@@ -0,0 +1,13 @@
+package com.thenovels.TheNovels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheNovelsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheNovelsApplication.class, args);
+	}
+
+}
