fed0151ff36341eff10006fef8e6120e220f4a66
Kokfong Hav <85272054+KokfongHav@users.noreply.github.com>
2024-12-31 14:18:43 +0700

    Complete category service and added exception

---
+++ src/main/java/com/thenovels/TheNovels/service/category/ICategoryService.java
@@ -0,0 +1,14 @@
+package com.thenovels.TheNovels.service.category;
+
+import com.thenovels.TheNovels.model.Category;
+
+import java.util.List;
+
+public interface ICategoryService {
+    Category getCategoryById(Long id);
+    Category getCategoryByName(String name);
+    List<Category> getAllCategories();
+    Category addCategory(Category category);
+    Category updateCategory(Category category, Long id);
+    void deleteCategoryById(Long id);
+}
