2a0e06dce07e6a5caba223e64f70813a25ddcc68
Kokfong Hav <havkokfong@hotmail.com>
2025-01-06 22:31:43 +0700

    Add Get Category controller

--- src/main/java/com/thenovels/TheNovels/controller/CategoryController.java
+++ src/main/java/com/thenovels/TheNovels/controller/CategoryController.java
@@ -1,4 +1,34 @@
 package com.thenovels.TheNovels.controller;
 
+import com.thenovels.TheNovels.model.Category;
+import com.thenovels.TheNovels.response.ApiResponse;
+import com.thenovels.TheNovels.service.category.ICategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("${api.prefix}/categories")
 public class CategoryController {
+    private final ICategoryService iCategoryService;
+
+    @GetMapping("/all")
+    public ResponseEntity<ApiResponse> getAllCategories() {
+        try {
+            List<Category> category = iCategoryService.getAllCategories();
+            return ResponseEntity.ok(new ApiResponse("Found!", category));
+        } catch (Exception e) {
+            return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse("Error:", INTERNAL_SERVER_ERROR));
+        }
+    }
+
+
 }
