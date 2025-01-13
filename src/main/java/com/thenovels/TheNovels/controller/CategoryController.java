bbfbd9237d60be723327752c871c4589e012774b
Kokfong Hav <85272054+KokfongHav@users.noreply.github.com>
2025-01-13 16:57:01 +0700

    category controller

--- src/main/java/com/thenovels/TheNovels/controller/CategoryController.java
+++ src/main/java/com/thenovels/TheNovels/controller/CategoryController.java
@@ -1,4 +1,51 @@
 package com.thenovels.TheNovels.controller;
 
+import com.thenovels.TheNovels.exception.AlreadyExistException;
+import com.thenovels.TheNovels.exception.ResourceNotFoundException;
+import com.thenovels.TheNovels.model.Category;
+import com.thenovels.TheNovels.response.ApiResponse;
+import com.thenovels.TheNovels.service.category.ICategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.*;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("${api.prefix}/categories")
 public class CategoryController {
+    private final ICategoryService iCategoryService;
+
+    @GetMapping("/all")
+    public ResponseEntity<ApiResponse> getAllCategories(){
+        try {
+            List<Category> categories = iCategoryService.getAllCategories();
+            return ResponseEntity.ok(new ApiResponse("Found!", categories));
+        } catch (Exception e) {
+            return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse("Error", INTERNAL_SERVER_ERROR));
+        }
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<ApiResponse> addCategory(@RequestBody Category name) {
+        try {
+            Category theCategory = iCategoryService.addCategory(name);
+            return ResponseEntity.ok(new ApiResponse("Success", theCategory));
+        } catch (AlreadyExistException e) {
+            return ResponseEntity.status(CONFLICT).body(new ApiResponse(e.getMessage(), null));
+        }
+    }
+
+    @GetMapping("/category/{id}/category")
+    public ResponseEntity<ApiResponse> getCategoryById(@PathVariable Long id){
+        try {
+            Category theCategory = iCategoryService.getCategoryById(id);
+            return ResponseEntity.ok(new ApiResponse("Found", theCategory));
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+        }
+    }
 }
